5631c47c12f8143f337f8ae153d77988a87db0ce
Cabicho <jodicabicho@hotmail.com>
2019-04-11 08:07:21 +0200

    config

--- meusite/settings.py
+++ meusite/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
+from decouple import config
+
+from dj_database_url import parse as dburl
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,14 +24,22 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = config('SECRET_KEY') #'s*8$8mo*v%hfzcx$=*#q8u765xe$6bzv=+jqxb)1ai6%rh0!jb'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = config('DEBUG', default=False, cast=bool) #True
+
+# https://dcimoz.herokuapp.com/
+# http://127.0.0.1:8000
+ALLOWED_HOSTS = ['dcimoz.herokuapp.com/','localhost','127.0.0.1']
+
+"""
+# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 's*8$8mo*v%hfzcx$=*#q8u765xe$6bzv=+jqxb)1ai6%rh0!jb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
-ALLOWED_HOSTS = []
-
-
+"""
 # Application definition
 
 INSTALLED_APPS = [
@@ -76,13 +88,22 @@ WSGI_APPLICATION = 'meusite.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+"""
+
+default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
+
+DATABASES = { 
+        'default': config('DATABASE_URL', default=default_dburl, cast=dburl), 
+}
+
+# Static files
 
 
 # Password validation
@@ -130,3 +151,5 @@ STATICFILES_DIRS = [
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MEDIA_URL = '/media/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
